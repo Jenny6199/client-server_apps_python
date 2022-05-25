e31c4b719dd9dce4c3b48cd40efdc39e70146371
Maksim_Sapunov <Jenny6199@yandex.ru>
2022-05-25 10:23:09 +0300

    definitions for server and client was prepared

---
+++ practical_tasks_lesson_3/mainapp/client.py
@@ -0,0 +1,30 @@
+def make_presence_message():
+    """
+    Формирует приветственное сообщение.
+    :return:
+    """
+    pass
+
+
+def send_message_to_server():
+    """
+    Отправляет сообщение на сервер.
+    :return:
+    """
+    pass
+
+
+def get_response_from_server():
+    """
+    Получает сообщение с сервера
+    :return:
+    """
+    pass
+
+
+def parse_server_message():
+    """
+    Осуществляет парсинг сообщения
+    :return:
+    """
+    pass
---
+++ practical_tasks_lesson_3/mainapp/server.py
@@ -0,0 +1,22 @@
+def get_client_message():
+    """
+    Принимает сообщение от клиента
+    :return:
+    """
+    pass
+
+
+def prepare_response():
+    """
+    Подготавливает ответное сообщение
+    :return:
+    """
+    pass
+
+
+def send_response():
+    """
+    Отправляет ответ клиенту
+    :return:
+    """
+    pass
