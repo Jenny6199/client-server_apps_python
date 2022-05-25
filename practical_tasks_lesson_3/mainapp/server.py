a72a1c384d2842b06535e6ed7a5fd85410797c6f
Maksim_Sapunov <jenny6199@yandex.ru>
2022-05-25 23:36:45 +0300

    add address param inside and change name of last def

--- practical_tasks_lesson_3/mainapp/server.py
+++ practical_tasks_lesson_3/mainapp/server.py
@@ -6,7 +6,7 @@ import sys
 import argparse
 import time
 
-from common.variables import ADDR, PORT_LISTEN
+from common.variables import ADDR_LISTEN, PORT_LISTEN
 
 
 def get_client_message():
@@ -33,28 +33,47 @@ def send_response():
     pass
 
 
-def get_port_for_use():
+def get_port_and_address_for_use():
     """
-    Осуществляет парсинг коммандной строки, возвращает номер порта
-    или ошибку.
+    Осуществляет парсинг коммандной строки, поиск номер порта,
+    и IP-адреса в параметрах, при их отсутствии возвращает
+    значения по умолчанию;
+    При наличии ошибок, возбуждает исключение и выводит на
+    дисплей текстовое сообщение.
+    :return: addr_listen - str: IP-addres
     :return: port_listen - str: port number
     """
     try:
-        parser = argparse.ArgumentParser(description='get port number')
+        parser = argparse.ArgumentParser(
+            description='Choose and indicate port number in range 1024-65535, '
+            'after -p parametr, f.e. -p 8080.'
+            'Then indicate IP address after -a parametr f.e. -a 127.0.0.1. '
+            'That will be use for socket create. '
+            'Default parameters will be choose when this options missing. '
+            'Exception and error message returns in case of mistake.'
+        )
         parser.add_argument('-p')
+        parser.add_argument('-a')
         args = parser.parse_args()
         port_listen = args.p
-        message = str(f'Используется заданный порт: {port_listen}')
+        addr_listen = args.a
+        message_port = f'Используется заданный порт: {port_listen}'
+        message_addr = f'Используется заданный IP: {addr_listen}'
         if port_listen is None:
             port_listen = PORT_LISTEN
-            message = str(f'Используется порт по умолчанию:{port_listen}')
+            message_port = f'Используется порт по умолчанию: {port_listen}'
         if int(port_listen) < 1024 or int(port_listen) > 65535:
             raise ValueError
-        print(message)
+        if addr_listen is None:
+            addr_listen = ADDR_LISTEN
+            message_addr = f'Используется IP-адрес по умолчанию: {addr_listen}'
+        print(message_addr, message_port, sep='\n')
+        
     except ValueError:
-        print('Номер порта может быть указано только в диапазоне от 1024 до 65535.')
+        print('Доступны для использования порты в диапазоне от 1024 до 65535.\n'
+              'Укажите иное значение порта, или используйте порт по умолчанию')
         sys.exit(1)
-    return port_listen
+    return addr_listen, port_listen
 
 
 def main():
@@ -62,7 +81,8 @@ def main():
     Агрегация работы функций и запуск программы-сервера.
     :return:
     """
-    get_port_for_use()
+    c = get_port_and_address_for_use()
+    print(c)
 
     return
 
