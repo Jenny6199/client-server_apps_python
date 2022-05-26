42f2a6caa46ea6adb3cd3a8cb530d4a4717e269d
Maksim_Sapunov <Jenny6199@yandex.ru>
2022-05-26 11:12:57 +0300

    filled def parse_server_message in client.py

--- practical_tasks_lesson_3/mainapp/client.py
+++ practical_tasks_lesson_3/mainapp/client.py
@@ -1,33 +1,40 @@
-def make_presence_message():
+from socket import *
+from common.utils import get_response, send_response
+from common.variables import ACTION, PRESENCE, TIME, USER, \
+    ACCOUNT_NAME, RESPONSE, ERROR, ADDR_LISTEN, PORT_LISTEN
+
+import sys
+import json
+import time
+
+
+def make_presence_message(account_name='Guest'):
     """
-    Формирует приветственное сообщение.
+    Формирует приветственное сообщение от клиента
+    :param account_name: string - name of client
+    :return: out: dict - message for server
+    """
+    out = {
+        ACTION: PRESENCE,
+        TIME: time.time(),
+        USER: {
+            ACCOUNT_NAME: account_name
+        }
+    }
+    return out
+
+
+def parse_server_message(message):
+    """
+    Осуществляет парсинг сообщения от сервера
+    :param message - json словарь с данными.
     :return:
     """
-    pass
-
-
-def send_message_to_server():
-    """
-    Отправляет сообщение на сервер.
-    :return:
-    """
-    pass
-
-
-def get_response_from_server():
-    """
-    Получает сообщение с сервера
-    :return:
-    """
-    pass
-
-
-def parse_server_message():
-    """
-    Осуществляет парсинг сообщения
-    :return:
-    """
-    pass
+    if RESPONSE not in message:
+        raise ValueError
+    if message[RESPONSE] == 200:
+        return '200: OK'
+    return f'400: {message[ERROR]}'
 
 
 def main():
