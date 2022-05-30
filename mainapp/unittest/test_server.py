8f4adec7c8d7afb5b5cebef29964e941d85df864
Maksim_Sapunov <Jenny6199@yandex.ru>
2022-05-30 09:20:06 +0300

    complite TestPrepareResponse

--- mainapp/unittest/test_server.py
+++ mainapp/unittest/test_server.py
@@ -1,10 +1,14 @@
+"""Тестирование серверной части программы"""
+
 import unittest
 
 # Обеспечиваем доступность пути к корню проекта
 import sys
 import os
 sys.path.append(os.path.join(os.getcwd(), '..'))
+# PyCharm грубо ругается на пути и переменные, импорт  но продолжает работать :)
 from server import prepare_response
+from common.variables import RESPONSE, ERROR
 
 
 class TestPrepareResponse(unittest.TestCase):
@@ -25,13 +29,55 @@ class TestPrepareResponse(unittest.TestCase):
         'user': {'name': 'Maksim'}
     }
 
+    error_result = {
+        RESPONSE: 400,
+        ERROR: 'Bad Request'
+    }
+
+    def test_response_not_None(self):
+        """
+        Проверка, что результат работы функции не None
+        """
+        self.assertIsNotNone(
+            prepare_response(self.right_message)
+        )
+
+    def test_response_is_json(self):
+        """
+        Проверка, что результат работы функции - словарь (json)
+        """
+        self.assertIsInstance(
+            prepare_response(self.right_message),
+            dict
+        )
+
     def test_with_right_message(self):
-        """Тестирование функции c правильным сообщением."""
-        self.assertEqual(prepare_response(self.right_message)['response'], 200)
+        """
+        Тестирование функции c правильным сообщением.
+        """
+        self.assertEqual(
+            prepare_response(self.right_message)['response'],
+            200
+        )
 
     def test_with_wrong_message(self):
-        """Тестирование функции с ошибочным сообщением."""
-        self.assertEqual(prepare_response(self.wrong_message)['response'], 400)
+        """
+        Тестирование функции с ошибочным сообщением.
+        """
+        self.assertEqual(
+            prepare_response(self.wrong_message)['response'],
+            400
+        )
+
+    def test_return_error(self):
+        """
+        Проверка возврата функцией ошибочного ответа
+        при получении неправильных данных
+        """
+        self.assertEqual(
+            prepare_response(self.wrong_message),
+            self.error_result
+        )
 
 
 if __name__ == '__main__':
