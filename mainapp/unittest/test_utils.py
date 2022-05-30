34e6d6e3f757c69db607c85f615a980d88de8ec5
Maksim_Sapunov <jenny6199@yandex.ru>
2022-05-30 18:46:07 +0300

    success_trouble_shooting

--- mainapp/unittest/test_utils.py
+++ mainapp/unittest/test_utils.py
@@ -11,26 +11,25 @@ import os
 import json
 sys.path.append(os.path.join(os.getcwd(), '..'))
 
-from common.utils import send_response, get_port_and_address_for_use
+from common.utils import send_response, get_response, get_port_and_address_for_use
 from common.variables import ENCODING_METHOD, RESPONSE, ERROR
 
 
 class TestSocket:
     """Имитация работы сокета"""
-    def __init__(self, message):
-        self.message = message
+    def __init__(self, test_message):
+        self.test_message = test_message
         self.encoded_message = None
         self.received_message = None
 
     def send(self, message):
         """
         Тестовая функция, аналог для проверки работы send_response.
+        в отличии от send_response не отправляет данные в сокет
         сохраняет данные в переменную для последующей проверки.
         """
-        if not isinstance(message, dict):
-            raise TypeError
-        json_message = json.dumps(message)
-        self.encoded_message = json_message.encode(ENCODING_METHOD)
+        json_test_message = json.dumps(self.test_message)
+        self.encoded_message = json_test_message.encode(ENCODING_METHOD)
         self.received_message = message
 
     def recv(self):
@@ -38,7 +37,7 @@ class TestSocket:
         Тестовая функция, имитирующая получение значений
         из сокета.
         """
-        json_test_message = json.dumps(self.message)
+        json_test_message = json.dumps(self.test_message)
         return json_test_message.encode(ENCODING_METHOD)
 
 
@@ -49,7 +48,7 @@ class TestSendResponse(unittest.TestCase):
 
     right_message = {
         'action': 'presence',
-        # 'time': 'Mon May 30 01:10:47 2022',
+        'time': 'Mon May 30 01:10:47 2022',
         'user': {'account_name': 'Guest'}
     }
 
@@ -65,27 +64,17 @@ class TestSendResponse(unittest.TestCase):
     def test_send_response_right_message(self):
         """Тестирование функции с корректным сообщением"""
         test_socket = TestSocket(self.right_message)
-        test_socket.send(self.right_message)
-        test_socket.encoded_message = test_socket
-        self.assertEqual(test_socket.encoded_message,
-                         test_socket.received_message)
-
-
-class TestGetPortAndAddressForUse(unittest.TestCase):
-    """
-    Тестирование функции get_port_and_address_for_use
-    """
-    DEFAULT_IP = 1000
-
-    def test_port_exception(self):
-        with self.assertRaises(ValueError):
-            get_port_and_address_for_use('server')
-
-    def test_return_is_tuple(self):
-        self.assertIsInstance(
-            get_port_and_address_for_use('server'),
-            tuple
+        send_response(test_socket, self.right_message)
+        self.assertEqual(
+            test_socket.encoded_message, 
+            test_socket.received_message
         )
+
+    def test_send_response_with_empty_message(self):
+        """Тестирование функции с некорректным (пустым) сообщением"""
+        test_socket = TestSocket(self.right_message)
+        with self.assertRaises(TypeError):
+            send_response(test_socket, '')
 
 
 if __name__ == '__main__':
