f37d16d6f9c8d4ab5a68b9e09a945c16d0d62451
Maksim_Sapunov <Jenny6199@yandex.ru>
2022-06-02 09:13:55 +0300

    current setup log-message

--- mainapp/server.py
+++ mainapp/server.py
@@ -8,6 +8,12 @@ from common.utils import get_response, send_response, \
 from common.variables import CONNECTION_LIMIT, \
     ACTION, ACCOUNT_NAME, USER, TIME, PRESENCE, \
     RESPONSE, ERROR, ALLOWED_USERS
+import logging
+import log.server_log_config
+
+# Инициализация журнала логирования сервера.
+# Имя регистратора должно соответствовать имени в server_log_config.py
+SERVER_LOG = logging.getLogger('server')
 
 
 def prepare_response(message):
@@ -21,11 +27,13 @@ def prepare_response(message):
             and TIME in message \
             and USER in message \
             and message[USER][ACCOUNT_NAME] in ALLOWED_USERS:
+        SERVER_LOG.debug('Получено корректное сообщение от клиента')
         return {
             RESPONSE: 200,
             'time': time.ctime(),
             'text': 'Hello client!'
         }
+    SERVER_LOG.debug('Получено ошибочное сообщение от клиента')
     return {
         RESPONSE: 400,
         ERROR: 'Bad Request'
@@ -42,18 +50,21 @@ def main():
     transport = socket(AF_INET, SOCK_STREAM)
     transport.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     transport.bind(option)
+    SERVER_LOG.debug(f'Запущен сервер ip-адрес: {option[0]}, порт: {option[1]}')
     # Режим ожидания входящих сообщений
+    SERVER_LOG.info('Сервер ожидает входящие сообщения.')
     transport.listen(CONNECTION_LIMIT)
     while True:
         client, client_address = transport.accept()
         try:
             message_from_client = get_response(client)
-            print(message_from_client)
+            SERVER_LOG.info(message_from_client)
             response = prepare_response(message_from_client)
             send_response(client, response)
+            SERVER_LOG.info('Отправлено сообщение клиенту')
             client.close()
         except (ValueError, json.JSONDecodeError):
-            print('Получено некорректное сообщение от клиента')
+            SERVER_LOG.warning('Получено некорректное сообщение от клиента')
             client.close()
 
 
