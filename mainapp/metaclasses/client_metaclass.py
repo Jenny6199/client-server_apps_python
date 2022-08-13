739dbcf09dcf3bad384efceb1802c145740ce174
Maksim Sapunov <jenny6199@yandex.ru>
2022-08-14 01:26:26 +0300

    create client_metaclass.py

---
+++ mainapp/metaclasses/client_metaclass.py
@@ -0,0 +1,25 @@
+import dis
+
+
+class ClientChecker(type):
+    def __init__(cls, clsname, bases, clsdict):
+        methods = []
+        for func in clsdict:
+            try:
+                ret = dis.get_instructions(clsdict[func])
+            except TypeError:
+                pass
+            else:
+                for i in ret:
+                    if i.opname == 'LOAD_GLOBAL':
+                        if i.argval not in methods:
+                            methods.append(i.argval)
+        for command in ('accept', 'listen', 'socket'):
+            if command in methods:
+                raise TypeError('Класс использует запреденные методы!')
+        if 'get_response' in methods or 'send_response' in methods:
+            pass
+        else:
+            raise TypeError('Отсутствуют функции работающие с сокетом!')
+
+        super.__init__(clsname, bases, clsdict)
