5d08d34ee663969d3eaa5d6294435b9aeb054644
Maksim Sapunov <jenny6199@yandex.ru>
2022-08-14 00:39:13 +0300

    create port_descr.py

---
+++ mainapp/descriptors/port_descr.py
@@ -0,0 +1,17 @@
+"""
+include class descriptor for port number
+"""
+from logging import getLogger
+logger = getLogger('server')
+
+
+class Port:
+    def __set__(self, instance, value):
+        if not 1023 < value < 65536:
+            logger.critical(f'Задан недопустимый номер порта {value}. '
+                            f'Выбирайте порт в диапазоне 1024-65535.')
+            exit(1)
+        instance.__dict__[self.name] = value
+
+    def __set_name__(self, owner, name):
+        self.name = name
