21041ceb3f506c930c26de07b17b35f73d45781a
Maksim <Jenny6199@yandex.ru>
2022-11-18 18:22:22 +0300

    add some comments

--- mainapp/db_builder/server_data_base.py
+++ mainapp/db_builder/server_data_base.py
@@ -65,14 +65,19 @@ class ServerDB:
                                    Column('ip', String)
                                    )
 
-        self.metadata.create_all(self.database_engine)  # Создание таблиц
+        # Создание таблиц
+        self.metadata.create_all(self.database_engine)
+        # Связывание таблиц БД с классами Python с помощью mapper
         mapper(self.AllUsers, users_table)
         mapper(self.ActiveUsers, active_users_table)
         mapper(self.UsersHistory, user_login_history)
 
+        # Создание сессии
         session = sessionmaker(bind=self.database_engine)
         self.session = session()
         self.session.query(self.ActiveUsers).delete()
+
+        # initial commit
         self.session.commit()
 
     def user_login(self, username, ip_address, port):
