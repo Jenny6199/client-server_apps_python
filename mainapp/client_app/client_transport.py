a9fcff40b4347391fe8e455bd5186923e8ea3104
Maksim <Jenny6199@yandex.ru>
2022-12-05 11:57:22 +0300

    add create_presense_message in transport.py

--- mainapp/client_app/client_transport.py
+++ mainapp/client_app/client_transport.py
@@ -7,6 +7,7 @@ import logging
 from PyQt5.QtCore import pyqtSignal, QObject
 
 from mainapp.common.utils import *
+from mainapp.common.variables import *
 from mainapp.common.errors import *
 
 logger = logging.getLogger('client')
@@ -48,10 +49,44 @@ class ClientTransport(threading.Thread, QObject):
         self.running = True
 
     def connection_init(self, port, ip_address):
-        pass
+        """Функция инициализации соединения с сервером"""
+        self.transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.transport.settimeout(5)
+        flag_connected = False
+        for i in range(5):
+            logger.info(f'Попытка соединения № {i+1}')
+            try:
+                self.transport.connect((ip_address, port))
+            except(OSError, ConnectionRefusedError):
+                pass
+            else:
+                flag_connected = True
+                break
+            time.sleep(1)
+        if not flag_connected:
+            logger.critical('Не удалось подключиться к серверу!')
+            raise ServerError('Не удалось подключиться к серверу!')
+        logger.debug('Установлено соединение с сервером')
 
     def user_list_update(self):
         pass
 
     def contact_list_update(self):
         pass
+
+    def create_presence_message(self):
+        """
+        Формирует приветственное сообщение от клиента
+        :return: out: dict - message for server
+        """
+        out = {
+            ACTION: PRESENCE,
+            TIME: time.ctime(),
+            USER: {
+                ACCOUNT_NAME: self.username
+            }
+        }
+        logger.debug(f'Сформировано {PRESENCE} сообщение для пользователя {self.username}.')
+        return out
+
+
